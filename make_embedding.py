cfd9595c96241c54999ae664b325b86b8f89e467
FMagnani <federico.magnani5@studio.unibo.it>
2022-04-26 12:47:40 +0200

    Added automatic testing in case of FB15k-237 dataset

--- make_embedding.py
+++ make_embedding.py
@@ -24,18 +24,20 @@ REGULARIZATION_COEF = config["make_embedding"]["RegularizationCoef"]
 GPU = config["make_embedding"]["GPU"]
 NUM_THREADS = config["make_embedding"]["NumThreads"]
 
-bash_command = "dglke_train --dataset "+EXPERIMENT_NAME 
 
-line_1 = "dglke_train --dataset \""+EXPERIMENT_NAME+"\" --data_path \""+EXPERIMENT_NAME+"/\" --save_path \""+EXPERIMENT_NAME+"/\" "
-line_2 = "--model_name \""+MODEL_NAME+"\" --data_files \"train.txt\" \"valid.txt\" \"test.txt\" --format \""+FORMAT+"\" "
-line_3 = "--lr "+str(LR)+" --max_step "+str(MAX_STEP)+" --log_interval "+str(LOG_INTERVAL)+" "
-line_4 = "--hidden_dim "+str(HIDDEN_DIM)+" "
-line_5 = "--batch_size "+str(BATCH_SIZE)+" --neg_sample_size "+str(NEG_SAMPLE_SIZE)+" "
-line_6 = "--loss_genre \"Logistic\" "
-line_7 = "--gpu "+str(GPU)+" --num_thread "+str(NUM_THREADS)+" --mix_cpu_gpu "
-line_8 = "--regularization_coef "+str(REGULARIZATION_COEF)+" -adv --gamma "+str(GAMMA)
+line_1 = "dglke_train --dataset \""+EXPERIMENT_NAME+"\" --data_path \""+EXPERIMENT_NAME+"/\" --save_path \""+EXPERIMENT_NAME+"/\""
+line_2 = " --model_name \""+MODEL_NAME+"\" --data_files \"train.txt\" \"valid.txt\" \"test.txt\" --format \""+FORMAT+"\""
+line_3 = " --lr "+str(LR)+" --max_step "+str(MAX_STEP)+" --log_interval "+str(LOG_INTERVAL)
+line_4 = " --hidden_dim "+str(HIDDEN_DIM)
+line_5 = " --batch_size "+str(BATCH_SIZE)+" --neg_sample_size "+str(NEG_SAMPLE_SIZE)
+line_6 = " --loss_genre \"Logistic\""
+line_7 = " --gpu "+str(GPU)+" --num_thread "+str(NUM_THREADS)+" --mix_cpu_gpu"
+line_8 = " --regularization_coef "+str(REGULARIZATION_COEF)+" -adv --gamma "+str(GAMMA)
 
 command = line_1 + line_2 + line_3 + line_4 + line_5 + line_6 + line_7 + line_8
+
+if EXPERIMENT_NAME=="FB15k-237":
+    command = command + " --test"
 
 subprocess.run(command, shell=True)
 
