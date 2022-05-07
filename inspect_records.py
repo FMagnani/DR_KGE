fa5f9ec7e878cc3760551c3b80b129213daffbc0
Federico Magnani <federico.magnani5@studio.unibo.it>
2022-05-07 19:09:21 +0200

    Fixed error and improved logging

--- inspect_records.py
+++ inspect_records.py
@@ -9,6 +9,7 @@ df = pd.DataFrame()
 total_exp = records['total_exp']
 
 model_list=[]
+model_number_list=[]
 dataset_list=[]
 max_step_list=[]
 batch_size_list=[]
@@ -21,6 +22,7 @@ for i in range(total_exp+1)[1:]:
     idx = str(i)
     exp = records[idx]
     model_list.append(exp['model'])
+    model_number_list.append(exp['no.'])
     dataset_list.append(exp['experiment_name'])
     max_step_list.append(exp['max_step'])
     batch_size_list.append(exp['batch_size'])
@@ -31,6 +33,7 @@ for i in range(total_exp+1)[1:]:
     hits100_list.append(exp['Hits@100'])
 
 df["Model"] = pd.Series(model_list)
+df["ModelNumber"] = pd.Series(model_number_list)
 df["Dataset"] = pd.Series(dataset_list)
 df["Train Epochs"] = pd.Series(max_step_list)
 df["Batch Size"] = pd.Series(batch_size_list)
@@ -41,13 +44,23 @@ df["Hits@50"] = pd.Series(hits50_list)
 df["Hits@100"] = pd.Series(hits100_list)
 
 df = df.sort_values(by=["Hits@10","Hits@50","Hits@100"], axis=0, ascending=[False,False,False])
+df = df.reset_index(drop=True)
 
 print(df)
 
 winner_name = str(df["Model"][0])
-winner_idx = str(df.reset_index()["index"][0])
+winner_idx = str(df["ModelNumber"][0])
 dataset = df["Dataset"][0]
 winner_path = dataset+"/"+winner_name+"_"+dataset+"_"+winner_idx+"/"
 winner_hits100 = pd.read_csv(winner_path+"HitsAt100.csv", index_col=0)
 print("\nBest model's predictions:")
+print("\n", winner_name, ":\n", hits100)
 print(winner_hits100)
+
+print("\nOther models predictions:")
+
+for item in [("DistMult", "0"), ("TransE_l1", "2"), ("ComplEx", "0")]:
+    name, idx = item
+    path = dataset+"/"+name+"_"+dataset+"_"+idx+"/"
+    hits100 = pd.read_csv(path+"HitsAt100.csv", index_col=0)
+    print("\n", name, ":\n", hits100)
